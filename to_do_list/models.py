d0fc59700b9d8666163ff0a5e9881618bc47ee9b
michal999 <71217112+mmyszak999@users.noreply.github.com>
2022-04-02 11:08:22 +0200

    Ignore 'pycache' directories and modify models

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,5 @@
+from statistics import mode
+from tkinter import CASCADE
 from django.db import models
 
 PRIORITIES = [
@@ -17,6 +19,7 @@ class List(models.Model):
 class Task(models.Model):
     body = models.TextField(null=False, blank=False, max_length=75)
     priority = models.CharField(max_length=3, choices=PRIORITIES)
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.body[0:50]
