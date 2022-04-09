6d1c162e943866ebfb8dd371553d8c31610a5e3f
michal999 <71217112+mmyszak999@users.noreply.github.com>
2022-04-09 11:14:23 +0200

    Add: add put and delete methods to view (API)

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('lists/', ListView.as_view()),
     path('lists/<str:pk>', ListDetailView.as_view()),
     path('lists/<str:pk>/tasks_in_list', TasksInListView.as_view()),
-    path('tasks/', TaskView.as_view())
+    path('tasks/', TaskView.as_view()),
+    path('tasks/<str:pk>/', TaskDetailView.as_view())
 ]
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -21,11 +21,24 @@ class ListView(APIView):
             return Response(serializer.data, status=200)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class ListDetailView(APIView):
     def get(self, request, pk=None, format=None):
         single_list = List.objects.get(pk=pk)
         serializer = ListSerializer(single_list, many=False)
         return Response(serializer.data)
+
+    def put(self, request, pk=None, format=None):
+        list = List.objects.get(pk=pk)
+        serializer = ListSerializer(list, many=False)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None, format=None):
+        list = List.objects.get(pk=pk)
+        list.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class TasksInListView(APIView):
     def get(self, request, pk=None, format=None):
@@ -44,11 +57,26 @@ class TaskView(APIView):
         print(request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=200)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class TaskDetailView(APIView):
+    def get(self, request, pk=None, format=None):
+        task = Task.objects.get(pk=pk)
+        serializer = TaskSerializer(task, many=False)
+        return Response(serializer.data)
     
+    def put(self, request, pk=None, format=None):
+        task = Task.objects.get(pk=pk)
+        serializer = TaskSerializer(Task, many=False)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk=None, format=None):
+        task = Task.objects.get(pk=pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
