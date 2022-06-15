f362c58a878eaf5294cd4ce94bf96d73f0210c06
michal999 <71217112+mmyszak999@users.noreply.github.com>
2022-06-15 19:26:23 +0200

    modify setup data and add tests linked with task model

--- to_do_list/tests/tests.py
+++ to_do_list/tests/tests.py
@@ -1,6 +1,6 @@
-import json
+from random import randint
+
 from django.contrib.auth.models import User
-from django.test import Client
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
@@ -22,9 +22,27 @@ class TestLoginSetUp(APITestCase):
             List(title="weekly_goals", owner=cls.test_user)
         ])
 
-    
+        cls.lists = Task.objects.bulk_create([
+            Task(body="milk", task_list=cls.lists[0], priority='C'),
+            Task(body="lemon", task_list=cls.lists[0], priority='E'),
+            Task(body="bread", task_list=cls.lists[0], priority='B'),
+
+            Task(body="double the overall income in 2 years", task_list=cls.lists[1], priority='A'),
+            Task(body="expand business to 3 continents", task_list=cls.lists[1], priority='D'),
+
+            Task(body="do the training 3 times", task_list=cls.lists[2], priority='B'),
+            Task(body="repair the chair", task_list=cls.lists[2], priority='D'),
+            Task(body="buy new shoes", task_list=cls.lists[2], priority='E'),
+        ])
+
+        cls.list_count = List.objects.all().count()
+        cls.task_count = Task.objects.all().count()
+
+        cls.list_pk = randint(1, cls.list_count)
+        cls.task_pk = randint(1, cls.task_count)
+
     def setUp(self):
-        self.client.force_login(self.test_user)
+        self.client.force_login(self.super_user)
 
     def test_create_list(self):
         self.user_data = {'title': 'my_list', 'owner': self.test_user.id}
@@ -33,11 +51,19 @@ class TestLoginSetUp(APITestCase):
         self.assertEqual(self.user_data['title'], str(List.objects.get(id=4)))
         
     def test_get_all_lists(self):
-        obj_count = List.objects.filter(owner=self.test_user).count()
         response = self.client.get(reverse('api:list-lists'))
-        self.assertEqual((len(response.data)), obj_count)
+        self.assertEqual((len(response.data)), self.list_count)
     
     def test_get_single_list(self):
-        self.obj = List.objects.get(id=3)
+        self.obj = List.objects.get(pk=self.list_pk)
         response = self.client.get(reverse('api:list-single-list', kwargs={'pk': self.obj.pk}))
+        self.assertEqual(response.data['id'], self.obj.pk)
+
+    def test_get_all_tasks(self):
+        response = self.client.get(reverse('api:task-tasks'))
+        self.assertEqual((len(response.data)), self.task_count)
+
+    def test_get_single_task(self):
+        self.obj = Task.objects.get(pk=self.task_pk)
+        response = self.client.get(reverse('api:task-single-task', kwargs={'pk': self.obj.pk}))
         self.assertEqual(response.data['id'], self.obj.pk)
